b866e3b34c6f30006f03b40d6edf374b1bc2b7dc
rattboi <rattboi@gmail.com>
2015-04-21 12:22:22 -0700

    Weekly post

---
+++ template/posts/2015-04-21-week2-wewlc.html.clj
@@ -0,0 +1,8 @@
+; @layout post
+; @title  Working Effectively with Legacy Code - Week 2
+; @tag    weekly 
+
+(p "Last week's reading consisted of the following chapters from " (link "Working Effectively with Legacy Code" "http://www.amazon.com/dp/0131177052")", by Michael Feathers:") 
+(ul (list (str "Chapter 4 - Seams") 
+          (str "Chapter 5 - Tools")))
+
