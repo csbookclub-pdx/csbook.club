19c4aa9554ac1e981d3d81afc0f311c25c01bf09
Bradon Kanyid <rattboi@gmail.com>
2015-04-21 23:47:02 -0700

    Adding wewct Week 3 weekly content

---
+++ template/posts/2015-04-21-week3-wewlc.html.clj
@@ -0,0 +1,8 @@
+; @layout post
+; @title  Working Effectively with Legacy Code - Week 3
+; @tag    weekly 
+
+(p "This week's reading consists of the following chapters from " (link "Working Effectively with Legacy Code" "http://www.amazon.com/dp/0131177052")", by Michael Feathers:") 
+(ul (list (str "Chapter 6 - I Don't Have Much Time and I Have to Change It") 
+          (str "Chapter 7 - It Takes Forever to Make a Change")))
+
