58c2a75566034eae4ec60ce26a3e92e99a7aef54
Jason Owen <jason.a.owen@gmail.com>
2015-08-23 15:53:15 -0700

    add CS paper on functional programming

---
+++ template/posts/2015-03-31-paper-why-functional.html.clj
@@ -0,0 +1,7 @@
+; @layout post
+; @title  Why Functional Programming Matters
+; @tag    papers
+
+(p "We read a CS paper entitled " (link "Why Functional Programming Matters"
+  "https://github.com/papers-we-love/papers-we-love/blob/master/functional_programming/why-functional-programming-matters.pdf")
+  ", by John Hughes.")
