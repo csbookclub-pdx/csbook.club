62dbf9d331b2666cf9ae5abb9f49b8c7eb58d1fb
Jason Owen <jason.a.owen@gmail.com>
2015-08-23 16:11:00 -0700

    read ch 14-15 of WEWLC

---
+++ template/posts/2015-06-09-week8-wewlc.html.clj
@@ -0,0 +1,9 @@
+; @layout post
+; @title  Working Effectively with Legacy Code - Week 8
+; @tag    weekly
+
+(p "This week's reading consists of the following chapters from " (link
+  "Working Effectively with Legacy Code"
+  "http://www.amazon.com/dp/0131177052")", by Michael Feathers:")
+(ul (list (str "Chapter 14 - Dependencies on Libraries Are Killing Me")
+          (str "Chapter 15 - My Application Is All API Calls")))
