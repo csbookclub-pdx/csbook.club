8a408501ffbd8e0e5213882aa1d23442d02038a2
Jason Owen <jason.a.owen@gmail.com>
2015-08-23 16:05:19 -0700

    read paper Game Theory

---
+++ template/posts/2015-05-27-paper-game-theory.html.clj
@@ -0,0 +1,7 @@
+; @layout post
+; @title  Game Theory
+; @tag    papers
+
+(p "This week's reading is a CS paper entitled " (link "Game Theory"
+  "https://mathoverflow.net/questions/162836/why-was-john-nashs-1950-game-theory-paper-such-a-big-deal")
+  ", by John Nash.")
