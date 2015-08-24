d6780d65fe3be477e66bf85b141d3278ac5f3a27
Jason Owen <jason.a.owen@gmail.com>
2015-08-23 17:30:52 -0700

    finish WEWLC

---
+++ template/posts/2015-07-28-finish-wewlc.html.clj
@@ -0,0 +1,10 @@
+; @layout post
+; @title  Working Effectively with Legacy Code - Complete
+; @tag    weekly
+
+(p "We've finished reading part II of " (link
+  "Working Effectively with Legacy Code"
+  "http://www.amazon.com/dp/0131177052")", by Michael Feathers, and will read
+  part III separately:")
+(ul (list (str "Chapter 25 - Dependency-Breaking Techniques")))
+(p "The book club is finished with our second book!")
