5e801461af2a2deda666e818c8325b93ed600732
Jason Owen <jason.a.owen@gmail.com>
2015-08-23 17:20:02 -0700

    read ch 19-20 of WEWLC

---
+++ template/posts/2015-06-23-week10-wewlc.html.clj
@@ -0,0 +1,10 @@
+; @layout post
+; @title  Working Effectively with Legacy Code - Week 10
+; @tag    weekly
+
+(p "This week's reading consists of the following chapters from " (link
+  "Working Effectively with Legacy Code"
+  "http://www.amazon.com/dp/0131177052")", by Michael Feathers:")
+(ul (list (str "Chapter 19 - My Project Is Not Object Oriented.
+                How Do I Make Safe Changes?")
+          (str "Chapter 20 - My Application Is All API Calls")))
