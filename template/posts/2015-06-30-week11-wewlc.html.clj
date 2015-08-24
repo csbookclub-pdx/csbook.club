491c902cce6cee08e88ff242850672c544a99a33
Jason Owen <jason.a.owen@gmail.com>
2015-08-23 17:23:37 -0700

    read ch21 of WEWLC

---
+++ template/posts/2015-06-30-week11-wewlc.html.clj
@@ -0,0 +1,8 @@
+; @layout post
+; @title  Working Effectively with Legacy Code - Week 11
+; @tag    weekly
+
+(p "This week's reading consists of the following chapter from " (link
+  "Working Effectively with Legacy Code"
+  "http://www.amazon.com/dp/0131177052")", by Michael Feathers:")
+(ul (list (str "Chapter 21 - I'm Changing the Same Code All Over the Place")))
