ee871cb7b753dc2dffff20168d3140af4cf78f5a
Jason Owen <jason.a.owen@gmail.com>
2015-10-11 15:17:50 -0700

    read ch 1-2 of Beautiful Code

---
+++ template/posts/2015-09-29-beautiful-w1.html.clj
@@ -0,0 +1,14 @@
+; @layout post
+; @title  Beautiful Code - Week 1
+; @tag    weekly
+
+(p "After reading "
+  (link "Codd's paper"
+        (post-title->url "Codd"))
+  ", we decided to start reading "
+  (link "Beautiful Code: Leading Programmers Explain How They Think"
+        "http://www.amazon.com/dp/0596510047")
+  ". Tuesday, October 6th, we will meet to discuss the first two chapters:"
+  )
+(ul (list (str "Chapter 1 - A Regular Expression Matcher, by Brian Kernighan")
+          (str "Chapter 2 - Subversion's Delta Editor: Interface as Ontology, by Karl Fogel")))
