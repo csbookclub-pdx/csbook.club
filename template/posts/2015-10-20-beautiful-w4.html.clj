657880a9ce8e7fda91ee6d31b734cffc583ec4bb
Jason Owen <jason.a.owen@gmail.com>
2015-10-20 19:20:50 -0700

    read ch 6-7 of Beautiful Code

---
+++ template/posts/2015-10-20-beautiful-w4.html.clj
@@ -0,0 +1,11 @@
+; @layout post
+; @title  Beautiful Code - Week 4
+; @tag    weekly
+
+(p "Today we discussed chapters 4 and 5 of "
+  (link "Beautiful Code: Leading Programmers Explain How They Think"
+        "http://www.amazon.com/dp/0596510047")
+  ". Tuesday, October 27th, we will meet to discuss the next two chapters:"
+  )
+(ul (list (str "Chapter 6 - Framework for Integrated Test: Beauty Through Fragility, by Michael Feathers")
+          (str "Chapter 7 - Beautiful Tests, by Alberto Savoia")))
