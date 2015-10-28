fa3cb038d538c21d892c1106188fec94eecccdde
Craig Meinschein <cmeinsch@gmail.com>
2015-10-27 18:31:14 -0700

    Add Beautiful Code Week 5

---
+++ template/posts/2015-10-27-beautiful-w5.html.clj
@@ -0,0 +1,10 @@
+; @layout post
+; @title  Beautiful Code - Week 5
+; @tag    weekly
+
+(p "Today we discussed chapter 6 of "
+  (link "Beautiful Code: Leading Programmers Explain How They Think"
+        "http://www.amazon.com/dp/0596510047")
+  ". Tuesday, November 3rd, we will meet to discuss the next chapter:"
+  )
+(ul (list (str "Chapter 7 - Beautiful Tests, by Alberto Savoia")))
