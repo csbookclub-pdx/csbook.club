6fa52df983016e72588e80e876b54a35f9435d86
Rattboi <rattboi@gmail.com>
2015-11-04 22:45:44 -0800

    Found another small mistake about next meetup date

--- template/posts/2015-11-03-beautiful-w6.html.clj
+++ template/posts/2015-11-03-beautiful-w6.html.clj
@@ -7,6 +7,6 @@
         "http://www.amazon.com/dp/0596510047")
   ", but due to various schedule conflicts were unable to.")
 
-(p "Tuesday, November 3rd, we will meet to discuss the next two chapters:")
+(p "Tuesday, November 10th, we will meet to discuss the next two chapters:")
 (ul (list (str "Chapter 8 - On-The-Fly Code Generation for Image Processing, by Charles Petzold")
           (str "Chapter 9 - Top Down Operator Precedence, by Douglas Crockford")))
