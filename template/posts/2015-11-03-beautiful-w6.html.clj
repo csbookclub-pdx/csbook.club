6118964a0007f1ed9a744cec5c63c2408e2c0d21
Jason Owen <jason.a.owen@gmail.com>
2015-11-04 22:16:42 -0800

    read ch 8-9 of Beautiful Code

---
+++ template/posts/2015-11-03-beautiful-w6.html.clj
@@ -0,0 +1,12 @@
+; @layout post
+; @title  Beautiful Code - Week 6
+; @tag    weekly
+
+(p "We intended to meet this week to discuss chapter 6 of "
+  (link "Beautiful Code: Leading Programmers Explain How They Think"
+        "http://www.amazon.com/dp/0596510047")
+  ", but due to various schedule conflicts were unable to.")
+
+(p "Tuesday, November 3rd, we will meet to discuss the next two chapters:")
+(ul (list (str "Chapter 8 - On-The-Fly Code Generation for Image Processing, by Charles Petzold")
+          (str "Chapter 9 - Top Down Operator Precedence, by Douglas Crockford")))
