a813537a27c733d9ac92ba8ab2af9af510616014
rattboi <rattboi@gmail.com>
2016-04-15 15:32:19 -0700

    Adding new post for 'What is Software Design?'

---
+++ template/posts/2016-04-12-what-is-software-design.html.clj
@@ -0,0 +1,15 @@
+; @layout post
+; @title  Article - What is Software Design?
+; @tag    weekly
+
+(p "Today we discussed the paper, "
+  (link "What is Software Design?"
+        "http://www.developerdotstar.com/mag/articles/reeves_design.html")
+  ". ")
+  
+(p "Tuesday, April 19th, we will start a new book, "
+  (link "Structure and Interpretation of Computer Programs"
+        "https://mitpress.mit.edu/sicp/")
+  ". We will be reading Chapter 1.1 for Tuesday. ") 
+
+(p "Wish us luck...")
