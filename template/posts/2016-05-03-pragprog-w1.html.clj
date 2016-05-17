1655a21ec6983691f8adef6df10a313325a8a3c6
Jason Owen <jason.a.owen@gmail.com>
2016-05-16 23:02:33 -0700

    switch to reading The Pragmatic Programmer

---
+++ template/posts/2016-05-03-pragprog-w1.html.clj
@@ -0,0 +1,17 @@
+; @layout post
+; @title  Pragmatic Programmer - Week 1
+; @tag    weekly
+
+(p "Over the last few weeks, we attempted to read "
+  (link "Structure and Interpretation of Computer Programs"
+        "https://mitpress.mit.edu/sicp/")
+  ". However, it turns out none of us has the bandwidth to do an MIT CS course"
+  " in addition to everything else we do.")
+
+(p "Instead, we decided we will read "
+   (link "The Pragmatic Programmer: From Journeyman to Master"
+         "http://pragprog.com/book/tpp/the-pragmatic-programmer")
+  ", by Andrew Hunt and David Thomas.")
+
+(p "On Tuesday, May 17, we will meet to discuss the first chapter:")
+(ul (list (str "Chapter 1. A Pragmatic Philosophy")))
