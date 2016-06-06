01644dd26a75c18c1fed3223c808117796963fc4
Bradon Kanyid <bkanyid@go2uti.com>
2016-06-06 16:40:57 -0700

    Added weeks 3 + 4 of Pragmatic Programmer

---
+++ template/posts/2016-05-24-pragprog-w3.html.clj
@@ -0,0 +1,10 @@
+; @layout post
+; @title  Pragmatic Programmer - Week 3
+; @tag    weekly
+
+(p "Today we discussed chapter 2 of "
+   (link "The Pragmatic Programmer: From Journeyman to Master"
+         "http://pragprog.com/book/tpp/the-pragmatic-programmer")
+  ". Tuesday, May 31st, we will meet to discuss the next chapter:")
+
+(ul (list (str "Chapter 3. The Basic Tools")))
---
+++ template/posts/2016-05-31-pragprog-w4.html.clj
@@ -0,0 +1,10 @@
+; @layout post
+; @title  Pragmatic Programmer - Week 4
+; @tag    weekly
+
+(p "Today we discussed chapter 3 of "
+   (link "The Pragmatic Programmer: From Journeyman to Master"
+         "http://pragprog.com/book/tpp/the-pragmatic-programmer")
+  ". Tuesday, June 7th, we will meet to discuss the next chapter:")
+
+(ul (list (str "Chapter 4. Pragmatic Paranoia")))
